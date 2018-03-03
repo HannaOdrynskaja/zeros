862040124c9631d989bada9bcea30f5231c666a7
Hanna Odrynskaja <36698550+HannaOdrynskaja@users.noreply.github.com>
2018-03-03 09:18:26 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var num = number;
+	var countDegr=0;
+	var res = num; 
+	var i = 1; 
+		while (num>=Math.pow(5,i)) {
+				res=Math.floor(num/Math.pow(5,i));
+				countDegr=countDegr+res;
+				i++;		
+		}	
+return countDegr;		
 }
